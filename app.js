e016d0f88f9799d64a1f95196fee3c1e76d74dca
Elle Wolfgher <ellewolfgher@gmail.com>
2021-11-28 19:25:04 -0300

    cadastro

--- app.js
+++ app.js
@@ -17,9 +17,15 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 
 //********************* Elle *************************/
+app.get('/cadastro', (req, res) => {
+  res.render('cadastro');
+});
+app.post('cadastro', (req, res) => {})
+
+
 
 //********************* Alex *************************/
 app.get('/viewsMedicos', (req, res) => {
