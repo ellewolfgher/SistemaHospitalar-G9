834026b058f1234cf170f0cc6b3e5fa53688cab2
Oscar <apenasoscar@gmail.com>
2021-12-01 17:37:58 -0300

    card adm + js de busca

---
+++ public/js/pesqMedicos.js
@@ -0,0 +1,31 @@
+function pesquisarMedico() {
+
+  var input, filter, card, nomeMedico, i, txtValue;
+
+  input = document.getElementById("input-pesquisa");
+  filter = input.value.toUpperCase();
+  card = document.getElementById("card-container");
+  conteudo = card.getElementById("card-conteudo");
+
+  for (i = 0; i < conteudo.length; i++) {
+
+    nomeMedico = conteudo[i].getElementById("medico")[0];
+
+    if (nomeMedico) {
+
+      txtValue = nomeMedico.textContent || nomeMedico.innerText;
+
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+
+        conteudo[i].style.display = "";
+
+      } else {
+
+        conteudo[i].style.display = "none";
+
+      }
+    }
+
+
+  }
+}
