085642a43d766ccd1c8a8bdf20b8192ad0e4fb10
alexsandre-sales <lexkogake@gmail.com>
2021-12-06 13:52:06 -0300

    comentado a acao, sem uso

--- controllers/adm-control.js
+++ controllers/adm-control.js
@@ -27,67 +27,64 @@ exports.cadastrar = (req, res) => {
     if (err) {
       return res.status(400).send('Erro de pesquisa no banco ' + err);
     }
-    res.render('views/pages/cadastro', {resultado});
+    res.render('views/pages/cadastro', { resultado });
   });
-  
 };
 
 exports.cadastrarMedico = (req, res) => {
+  // const acaoo = 'Cadastrado com sucesso';
   if (req.body.idMed == '') {
     const save_medico = new db_medico();
 
-      save_medico.pronome = req.body.pronome;
-      save_medico.profile = req.body.profile;
-      save_medico.nome = req.body.nome;
-      save_medico.sobrenome = req.body.sobrenome;
-      save_medico.crm = req.body.crm;
-      save_medico.email = req.body.email;
-      save_medico.cel = req.body.cel;
-      save_medico.tel = req.body.tel;
-      save_medico.ramal = req.body.ramal;
-      save_medico.espec = req.body.especNome;
-  
-      save_medico.save((err) => {
-          if(err) throw err;
-  
-          return res.redirect('/view/cadastro')
-      })
-  } else { 
-      const id = req.body.idMed;
-      db_medico.findById(id, (erro, resultado)=>{
-          if(erro)throw erro
-          resultado.pronome = req.body.pronome;
-          resultado.profile = req.body.profile;
-          resultado.nome = req.body.nome;
-          resultado.sobrenome = req.body.sobrenome;
-          resultado.crm = req.body.crm;
-          resultado.email = req.body.email;
-          resultado.cel = req.body.cel;
-          resultado.tel = req.body.tel;
-          resultado.ramal = req.body.ramal;
-          resultado.espec = req.body.especNome;
-  
-          resultado.save((erro) => {
-              if(erro)throw erro
-              return res.redirect('/view/viewAdm')
-          })
-           
-      })
+    save_medico.pronome = req.body.pronome;
+    save_medico.profile = req.body.profile;
+    save_medico.nome = req.body.nome;
+    save_medico.sobrenome = req.body.sobrenome;
+    save_medico.crm = req.body.crm;
+    save_medico.email = req.body.email;
+    save_medico.cel = req.body.cel;
+    save_medico.tel = req.body.tel;
+    save_medico.ramal = req.body.ramal;
+    save_medico.espec = req.body.especNome;
+
+    save_medico.save(err => {
+      if (err) throw err;
+
+      return res.redirect('/view/cadastro');
+    });
+  } else {
+    const id = req.body.idMed;
+    db_medico.findById(id, (erro, resultado) => {
+      if (erro) throw erro;
+      resultado.pronome = req.body.pronome;
+      resultado.profile = req.body.profile;
+      resultado.nome = req.body.nome;
+      resultado.sobrenome = req.body.sobrenome;
+      resultado.crm = req.body.crm;
+      resultado.email = req.body.email;
+      resultado.cel = req.body.cel;
+      resultado.tel = req.body.tel;
+      resultado.ramal = req.body.ramal;
+      resultado.espec = req.body.especNome;
+
+      resultado.save(erro => {
+        if (erro) throw erro;
+        return res.redirect('/view/viewAdm');
+      });
+    });
   }
 };
 
 // Editar médico
 exports.editar = (req, res) => {
- 
   db_medico.findById(req.params.id, (err, resultado) => {
-    if(err) throw err;
-    db_espec.find({}, (err, especs)=> {
-      if(err) throw err;
-      return res.render('views/pages/edita', {resultado, especs});
-    })
-    
-  })
-}
+    if (err) throw err;
+    db_espec.find({}, (err, especs) => {
+      if (err) throw err;
+      return res.render('views/pages/edita', { resultado, especs });
+    });
+  });
+};
 
 // Deletar médico
 
@@ -97,5 +94,3 @@ exports.deletar = (req, res) => {
     return res.redirect('/view/viewAdm');
   });
 };
-
-
