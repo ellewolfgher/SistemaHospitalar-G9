fd1d3dde7944e43cfcefbde37476c0e382d59d46
Oscar <apenasoscar@gmail.com>
2021-12-06 00:21:52 -0300

    cards atlz + pesquisa

--- public/js/pesqMedicos.js
+++ public/js/pesqMedicos.js
@@ -1,45 +1,37 @@
-// function pesquisarMedico() {
-//   var input, filter, card, nomeMedico, i, txtValue;
-
-//   input = document.getElementById('input-pesquisa');
-//   filter = input.value.toUpperCase();
-//   card = document.getElementById('card-conteudo');
-//   let conteudo = card.getElementsByTagName('div');
-
-//   for (i = 0; i < conteudo.length; i++) {
-//     nomeSpec = conteudo[i].getElementsByTagName('span')[0];
-//     if (nomeSpec) {
-//       txtValue = nomeSpec.textContent || nomeSpec.innerText;
-
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         console.log(txtValue);
-//         console.log(conteudo[i]);
-//         conteudo[i].style.display = '';
-//         console.log(conteudo[i]);
-//       } else {
-//         console.log(conteudo[i]);
-//         conteudo[i].style.display = 'none';
-//         console.log(conteudo[i]);
-//       }
-//     }
-//   }
-// }
-
+//pesquisa por médico
 function pesquisarMedico() {
-  let input, filter, cards, cardContainer, i, txtValue;
 
-  input = document.getElementById('input-pesquisa');
-  filter = input.value.toUpperCase();
-  cardContainer = document.getElementById('teste');
-  cards = cardContainer.getElementsByClassName('card');
+  let input, cardContainer, cards, i;
 
-  for (i = 0; i < cards.length; i++) {
-    let nomeMedico = cards[i].querySelector('.espec');
-    if (nomeMedico.innerText.toUpperCase().indexOf(filter) > -1) {
-      cards[i].style.display = '';
-    } else {
-      cards[i].style.display = 'none';
+  input = document.getElementById('filter-medico').value.toUpperCase()
+  cardContainer = document.getElementById('card-container')
+  cards = cardContainer.getElementsByClassName('card')
+
+  for(i = 0; i < cards.length; i++) {
+    let title = cards[i].querySelector('.card-body h4.card-title')
+    if(title.innerText.toUpperCase().indexOf(input) > -1) {
+      cards[i].style.display = "";
+    }else{
+      cards[i].style.display = "none";
     }
   }
 }
-// }
+
+
+//pesquisa por especialidade
+function pesquisarEspec() {
+  let input, cardContainer, cards, i;
+
+  input = document.getElementById('filter-espec').value.toUpperCase()
+  cardContainer = document.getElementById('card-container')
+  cards = cardContainer.getElementsByClassName('card')
+
+  for(i = 0; i < cards.length; i++) {
+    let title = cards[i].querySelector('.card-body li.espec')
+    if(title.innerText.toUpperCase().indexOf(input) > -1) {
+      cards[i].style.display = "";
+    }else{
+      cards[i].style.display = "none";
+    }
+  }
+}
