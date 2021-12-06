40445e543a930cb1ab31063a320d224ebfb97d7d
Oscar <apenasoscar@gmail.com>
2021-12-06 00:48:30 -0300

    atlz card user + filtro OK

--- public/js/pesqMedicos.js
+++ public/js/pesqMedicos.js
@@ -1,14 +1,14 @@
 //pesquisa por médico
 function pesquisarMedico() {
 
-  let input, cardContainer, cards, i;
+  let input, cardContainer, cards, title, i;
 
   input = document.getElementById('filter-medico').value.toUpperCase()
   cardContainer = document.getElementById('card-container')
   cards = cardContainer.getElementsByClassName('card')
 
   for(i = 0; i < cards.length; i++) {
-    let title = cards[i].querySelector('.card-body h4.card-title')
+    title = cards[i].querySelector('.card-body h4.card-title')
     if(title.innerText.toUpperCase().indexOf(input) > -1) {
       cards[i].style.display = "";
     }else{
@@ -20,14 +20,15 @@ function pesquisarMedico() {
 
 //pesquisa por especialidade
 function pesquisarEspec() {
-  let input, cardContainer, cards, i;
+
+  let input, cardContainer, cards, title,i;
 
   input = document.getElementById('filter-espec').value.toUpperCase()
   cardContainer = document.getElementById('card-container')
   cards = cardContainer.getElementsByClassName('card')
 
   for(i = 0; i < cards.length; i++) {
-    let title = cards[i].querySelector('.card-body li.espec')
+    title = cards[i].querySelector('.card-body li.espec')
     if(title.innerText.toUpperCase().indexOf(input) > -1) {
       cards[i].style.display = "";
     }else{
